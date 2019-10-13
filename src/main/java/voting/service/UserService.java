644fd688788462b7faa69a78a78404ba227770b6
Sergey Kuzenny <kuzenny4@gmail.com>
2019-10-13 20:11:58 +0300

    5 REST controller

--- src/main/java/voting/service/UserService.java
+++ src/main/java/voting/service/UserService.java
@@ -3,16 +3,9 @@ package voting.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-import voting.AuthorizedUser;
 import voting.model.User;
 import voting.repository.UserRepository;
 
